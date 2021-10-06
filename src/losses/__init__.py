dfc347aa61d4b0c48f31ae5a5dfec899d7886c79
HeHaoming <hehaoming110@live.com>
2021-10-06 18:52:17 +0800

    1. 封装了两个loss的合并

--- src/losses/__init__.py
+++ src/losses/__init__.py
@@ -1,4 +1,15 @@
-from .dice_loss import *
+import torch.nn as nn
+
 from .binary_crossentropy import *
+from .dice_loss import *
 
 
+class HybridLoss(nn.Module):
+
+    def __init__(self):
+        super(HybridLoss, self).__init__()
+        self.dice = BinaryDiceLoss()
+        self.bce = BalancedBinaryCrossEntropy()
+
+    def forward(self, pred, target):
+        return self.dice(pred, target) + self.bce(pred, target)
